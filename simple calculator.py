f2a1e73c0b8915c0e5f5d5f609762e43798fc246
KayyoungHL <kayyounghl@gmail.com>
2021-11-20 21:55:02 +0900

    SECOND COMMIT

---
+++ simple calculator.py
@@ -0,0 +1,10 @@
+from my_adder import my_adder
+
+def main():
+    print(my_adder(3, 5))
+    print(my_adder(6, 3))
+    print(my_adder(-3, 7))
+    print(my_adder(0, -7))
+
+if __name__=="__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,8 @@
+def greeting():
+    print("hello world!")
+
+def main():
+    greeting()
+
+if __name__=="__main__":
+    main()
